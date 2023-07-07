820b2864f3124b56229ecfd42806448b49f8222b
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-07-07 23:26:23 +0100

    implement simd and a bunch of tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(portable_simd)]
+
 use std::ffi::CStr;
 
 pub fn count_iter(s: &CStr) -> isize {
@@ -14,6 +16,41 @@ pub fn count_iter(s: &CStr) -> isize {
 pub fn count_for_loop(s: &CStr) -> isize {
     let mut result = 0;
     for &c in s.to_bytes() {
+        if c == b'p' {
+            result += 1;
+        } else if c == b'n' {
+            result -= 1;
+        }
+    }
+
+    result
+}
+
+pub fn count_simd(s: &CStr) -> isize {
+    use std::simd::{u8x16, SimdInt, SimdPartialEq};
+    let bytes = s.to_bytes();
+    let (prefix, middle, suffix) = bytes.as_simd();
+
+    let p = u8x16::splat(b'p');
+    let n = u8x16::splat(b'n');
+
+    let mut result = 0;
+    for &window in middle {
+        let ps = window.simd_eq(p);
+        let ns = window.simd_eq(n);
+        let neg_ps = ps.to_int();
+        let neg_ns = ns.to_int();
+        let pairwise = neg_ns.saturating_add(-neg_ps);
+
+        result += pairwise.reduce_sum() as isize;
+    }
+
+    _count_scalar(prefix) + result + _count_scalar(suffix)
+}
+
+pub fn _count_scalar(s: &[u8]) -> isize {
+    let mut result = 0;
+    for &c in s {
         if c == b'p' {
             result += 1;
         } else if c == b'n' {
@@ -41,21 +78,57 @@ mod tests {
     use super::*;
     use std::ffi::CString;
 
+    const SMALL_SENTENCE: &str = "ppnpnpp";
+    const SMALL_SENTENCE_ANSWER: isize = 3;
+    const BIG_SENTENCE: &str = "pppppnpppnpppnn.npppnpnnnpnnnnpn";
+    const BIG_SENTENCE_ANSWER: isize = 3;
+
     #[test]
     fn it_works_iter() {
-        let sentence = CString::new("ppnpnpp").unwrap();
-        assert_eq!(3, count_iter(sentence.as_c_str()));
+        let sentence = CString::new(SMALL_SENTENCE).unwrap();
+        assert_eq!(SMALL_SENTENCE_ANSWER, count_iter(sentence.as_c_str()));
+    }
+
+    #[test]
+    fn it_works_iter_big() {
+        let sentence = CString::new(BIG_SENTENCE).unwrap();
+        assert_eq!(BIG_SENTENCE_ANSWER, count_iter(sentence.as_c_str()));
     }
 
     #[test]
     fn it_works_for_loop() {
         let sentence = CString::new("ppnpnpp").unwrap();
-        assert_eq!(3, count_for_loop(sentence.as_c_str()));
+        assert_eq!(SMALL_SENTENCE_ANSWER, count_for_loop(sentence.as_c_str()));
+    }
+
+    #[test]
+    fn it_works_for_loop_big() {
+        let sentence = CString::new(BIG_SENTENCE).unwrap();
+        assert_eq!(BIG_SENTENCE_ANSWER, count_for_loop(sentence.as_c_str()));
     }
 
     #[test]
     fn it_works_c() {
         let sentence = CString::new("ppnpnpp").unwrap();
-        assert_eq!(3, count_c(sentence.as_c_str()));
+        assert_eq!(SMALL_SENTENCE_ANSWER, count_c(sentence.as_c_str()));
+    }
+
+    #[test]
+    fn it_works_c_big() {
+        let sentence = CString::new(BIG_SENTENCE).unwrap();
+        assert_eq!(BIG_SENTENCE_ANSWER, count_c(sentence.as_c_str()));
+    }
+
+    #[test]
+    fn it_works_simd() {
+        let sentence = CString::new("ppnpnpp").unwrap();
+        // will not actually use SIMD:
+        assert_eq!(SMALL_SENTENCE_ANSWER, count_simd(sentence.as_c_str()));
+    }
+
+    #[test]
+    fn it_works_simd_big() {
+        let sentence = CString::new(BIG_SENTENCE).unwrap();
+        assert_eq!(BIG_SENTENCE_ANSWER, count_simd(sentence.as_c_str()));
     }
 }
