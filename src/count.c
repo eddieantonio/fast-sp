ca28cba109024aa1d4ad2472f6f5625a30b0b9bc
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-07-09 17:45:47 +0100

    change: make signature match owen's original

--- src/count.c
+++ src/count.c
@@ -1,13 +1,7 @@
-#include <assert.h>
-#include <stddef.h>
 #include <stdint.h>
 
-// ssize_t is not a part of the C standard, so we have to define it:
-typedef uint64_t ssize_t;
-static_assert(sizeof(ssize_t) == sizeof(size_t), "Not the right size, whoops");
-
-ssize_t count_c(const char *s) {
-    ssize_t result = 0;
+int64_t count_c(const char *s) {
+    int64_t result = 0;
     while (*s != '\0') {
         switch (*s) {
             case 's':
