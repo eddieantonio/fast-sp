fd4879addb5f86ae19818b80b791ad5fb07968fc
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-07-09 14:47:40 +0100

    [wip] generate random data

--- generate-test-data.py
+++ generate-test-data.py
@@ -11,20 +11,14 @@ SIZE = 12 * MEGABYTE
 generator = np.random.default_rng()
 
 
-def all_p():
-    return np.full((SIZE,), ord("p"), dtype=np.uint8)
-
-
-def all_s():
-    return np.full((SIZE,), ord("s"), dtype=np.uint8)
-
-
 def random_sp():
+    "Generate an array of random ASCII 's' or 'p' characters"
     s_or_p = np.array([ord("p"), ord("s")], dtype=np.uint8)
     return generator.choice(s_or_p, size=(SIZE,), shuffle=False)
 
 
 def random_ascii_printable():
+    "Generate an array of random printable ASCII characters"
     min_char = ord(" ")  # Lowest, printable ASCII char
     max_char = ord("~")  # Highest, printable ASCII char
     return generator.integers(
@@ -33,7 +27,7 @@ def random_ascii_printable():
 
 
 with open("random-printable.bin", "wb") as data_file:
-   random_ascii_printable().tofile(data_file)
+    random_ascii_printable().tofile(data_file)
 
 with open("random-sp.bin", "wb") as data_file:
-   random_sp().tofile(data_file)
+    random_sp().tofile(data_file)
