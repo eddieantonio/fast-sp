86e1cd7d286c9b96acda54020e6640e7476d0eb1
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-07-17 16:04:37 +0100

    docs(rust): mostly tweak the C FFI docs

--- src/implementations/c_ffi.rs
+++ src/implementations/c_ffi.rs
@@ -16,7 +16,7 @@
 use std::ffi::{c_char, CStr};
 
 macro_rules! define_ffi {
-    ($(#[$meta:meta])* $name: ident => $link_name: ident) => {
+    ($(#[$meta:meta])* $name: ident => $link_name: ident *const c_char) => {
         $(#[$meta])*
         #[inline(always)]
         pub fn $name(s: &CStr) -> isize {
@@ -36,8 +36,6 @@ macro_rules! define_ffi {
         $(#[$meta])*
         #[inline(always)]
         pub fn $name(s: &CStr) -> isize {
-            // Tiny wrapper that changes Rust's borrowed CStr and converts it into C's const char*.
-            // This will link to libcount.a and use its count_c function.
             // The scope of this external symbol is entirely internal to this function.
             #[link(name = "count", kind = "static")]
             extern "C" {
@@ -51,26 +49,29 @@ macro_rules! define_ffi {
 }
 
 define_ffi!(
-    /// Count implementation written in C. See src/count.c
-    c_while_loop => while_not_zero
+    /// Owen's original implementation written in C. See c/original.c
+    c_original => run_switches *const c_char
 );
 
 define_ffi!(
-    /// Owen's original implementation written in C. See src/count.c
-    c_original => run_switches
+    /// Count implementation written in C. See c/while-not-zero.c
+    c_while_loop => while_not_zero *const c_char
 );
 
 define_ffi!(
     /// Owen's implementation, with explicit size (does not check for null terminator).
+    /// See c/with-explicit-size.c
     c_for_loop => with_explicit_size usize
 );
 
 define_ffi!(
     /// Using a state machine approach from <https://github.com/robertdavidgraham/wc2>
+    /// See c/state-machine.c
     c_state_machine => c_state_machine usize
 );
 
 define_ffi!(
     /// Like the [c_state_machine], but without the state machine.
+    /// See c/count-machine.c
     c_count_machine => c_count_machine usize
 );
